462f2aedbdfd75e6705cfb4742c9483364059093
Ezall2001 <armenre2001@outlook.fr>
2021-07-14 07:28:26 +0100

    refactored the contact-service.js

--- public/app/contact-service.js
+++ public/app/contact-service.js
@@ -1,38 +1,73 @@
-jQuery(() => {
+const getInput = (fields = []) => {
 
+  return fields
+    .map(field => ({
+      name: $(field).attr('name'),
+      value: $(field).val()
+    }))
+}
+
+const getMissing = (input = []) => {
+
+  return input
+    .filter(input => input.value == '')
+    .map(input => input.name)
+}
+
+const getInvalid = (input = []) => {
+
+  input
+    .filter(input => {
+
+      if (input.name == 'tel') {}
+    })
+
+}
+
+let err = null
+
+const errHandler = (errorName, errors = []) => {
+
+
+  if (errors.length > 0) {
+
+    err = `these fields are ${errorName}: <i>`
+    errors.forEach(input => {
+      err += input + ' - '
+      $(`input[name=${input}],textarea[name=${input}]`)
+        .addClass('error')
+    })
+
+    err = err.slice(0, err.length - 3) + '</i>'
+
+    alertPop.err(err)
+
+    return err
+  }
+
+
+
+}
+
+jQuery(() => {
   $('section.contact button[type=submit]')
     .on('click', event => {
+
       event.preventDefault()
 
-      const input = $('.contact input')
+      const fields = $('.contact input, .contact textarea')
         .toArray()
-        .map(input => ({
-          name: $(input).attr('name'),
-          value: $(input).val()
-        }))
 
-      input.push({
-        name: $('.contact textarea').attr('name'),
-        value: $('.contact textarea').val()
-      })
+      const input = getInput(fields)
+      const missing = getMissing(input)
+      const invalid = getInvalid(input)
 
-      const missing = input
-        .filter(input => input.value == '')
-        .map(input => input.name)
 
-      let err = null
+      err = errHandler('missing', missing)
+      err = errHandler('invalid', invalid)
 
-      if (missing.length > 0) {
-        err = 'these fields are missing: <i>'
-        missing.forEach(input => {
-          err += input + ' - '
-          $(`input[name=${input}],textarea[name=${input}]`)
-            .addClass('missing')
-        })
-        err = err.slice(0, err.length - 3)
-        err += '</i>'
-        alertPop.err(err)
-      }
+
+
 
       // TODO: add the ok status and refactor this
 
@@ -40,26 +75,26 @@ jQuery(() => {
 
 
 
-      if (err == null) {
+      // if (err == null) {
 
-        $.ajax({
-          method: 'POST',
-          url: 'https://formsubmit.co/ajax/armen.bakir@esprit.tn',
-          dataType: 'json',
-          accepts: 'application/json',
-          data: {
-            _captcha: 'false',
-            _template: 'table',
-            email: input[0].value,
-            phone: input[1].value,
-            message: input[2].value
-          },
-          success: (data) => alertPop.success("Email sent"),
-          error: (err) => alertPop.err(`sorry for your inconvience, this service is down temporarily.
-          </br> please contact us on <u>lionsclubsirius@gmail.com</u>
-          `)
-        })
-      }
+      //   $.ajax({
+      //     method: 'POST',
+      //     url: 'https://formsubmit.co/ajax/armen.bakir@esprit.tn',
+      //     dataType: 'json',
+      //     accepts: 'application/json',
+      //     data: {
+      //       _captcha: 'false',
+      //       _template: 'table',
+      //       email: input[0].value,
+      //       phone: input[1].value,
+      //       message: input[2].value
+      //     },
+      //     success: (data) => alertPop.success("Email sent"),
+      //     error: (err) => alertPop.err(`sorry for your inconvience, this service is down temporarily.
+      //     </br> please contact us on <u>lionsclubsirius@gmail.com</u>
+      //     `)
+      //   })
+      // }
 
     })
 
